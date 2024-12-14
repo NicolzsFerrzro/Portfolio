711edea72d79f32c11012415795a53fcd3d23614
NeroFiasconi <FIASCONI.NERO@GMAIL.COM>
2024-12-14 16:05:58 +0100

    dynamique accueil + correction formulaire

--- projet-num-boost/script.js
+++ projet-num-boost/script.js
@@ -1,20 +1,45 @@
-document.querySelector(".texteIndex").addEventListener("mouseover", function (bande1) {
-    
-    document.querySelector(".texteIndex").style.display = "none";
-    document.querySelector(".asideIndex2 img").style.display = "none";
-    document.querySelector(".texteIndex2").style.display = "block";
-    document.querySelector(".boxIMG img").style.display = "block";
-            "J'en suis venu au développement web par l'intermédiaire du pentesting, en m'interessant à la lecture de puces RFID. Des outils comme le ProxMark sont opensource et mis à jour constament sur GitHubet j'ai trouvé ça fascninant.J'ai commencé par du langage COBOL. Avec youtube j'ai appris à faire de petits programmes de calculs, calculs d'imc, calculs de vitesse moyenne en course à pied. Je me suis ensuite tourné vers le langage Python, la syntaxe est simple et intuitive ce qui facilite l'apprentissage des bases à savoir les fonctions, les conditions, les boucles ainsi que l'utilisation des collections. J'ai appris énormément sur l'algorithmie.Mais ces programmes ne fonctionnaient que dans la console, je voulais faire des applications qu'on pourrait utiliser sur une page web.j'ai donc appris les bases HTML, CSS et Javascript avec Openclassroom afin des faire des pages dynamiques. J'ai pû aborder facilement l'algorithmie en Javascript car la logique est là même qu'en Python cependant, la séléction des éléments du DOM pour en faire des objets Javascript fut une découverte.";
-        // document.querySelector(".aside img").style.display = "none";
-        // document.querySelector(".box IMG img").style.display = "block";
+document.getElementById("boutonSuivant").addEventListener("click", () => {
+    // Remplacer texte et image du conteneur
+        document.querySelector(".containerIndex").innerHTML =
+                    `<div class="imgIndex2">
+                <img src="image/20220122_095531.jpg" alt="Vieille prison sur une ile de banniare en guyane">
+            </div>
+            <div class="texteIndex2">
+            <h1>Parcours</h1>
+            <p>J'en suis venu au développement web par l'intermédiaire du pentesting,
+                en m'interessant à la lecture de puces RFID. Des outils comme le ProxMark sont opensource et mis à jour constament sur GitHub
+                et j'ai trouvé ça fascninant.
+                J'ai commencé par du langage COBOL. Avec youtube j'ai appris à faire de petits programmes de calculs, calculs d'imc, calculs de vitesse moyenne en course à pied. 
+                Je me suis ensuite tourné vers le langage Python, la syntaxe est simple et intuitive ce qui facilite l'apprentissage des bases à savoir les fonctions, 
+                les conditions, les boucles ainsi que l'utilisation des collections. J'ai appris énormément sur l'algorithmie.
+                Mais ces programmes ne fonctionnaient que dans la console, je voulais faire des applications qu'on pourrait utiliser sur une page web.
+                j'ai donc appris les bases HTML, CSS et Javascript avec Openclassroom afin des faire des pages dynamiques.</a></p>
+            </div>`
+        
 })
-
-document.querySelector(".texteIndex2").addEventListener("mouseover", function (bande2) {
-    
-    document.querySelector(".texteIndex2").style.display = "none";
-    document.querySelector(".boxIMG img").style.display = "none";
-    document.querySelector(".texteIndex").style.display = "block";
-    document.querySelector(".asideIndex2 img").style.display = "block";
+document.getElementById("boutonPrecedent").addEventListener("click", () => {
+    // Remplacer texte et image du conteneur
+    document.querySelector(".containerIndex").innerHTML =
+                `<div class="texteIndex">
+        <h1>Présentation</h1>
+        <p>Ancien militaire dans l'armée de terre et titulaire d'un DUT techniques de commercialisation, 
+            je souhaite me reconvertir en tant que concepteur d'application web.
+            Le role du concepteur est de répondre au mieux aux besoins du client 
+            en créant des applications sur mesure en abordant des themes importants 
+            comme l'experience utilisateur, les fonctionnalités, les bases de données, la sécurité. 
+            Pour y parvenir je me forme en autodidacte grâce à des plateformes comme Udémy, Openclassroom ainsi qu'IBM skillsbuild pour les fondamentaux du web. 
+            Suite à ces différentes formations je suis capable de faire des page web dynamique avec HTML, CSS et Javascript, et écrire des scripts en Python.</p>
+        </div>
+        <div class="imgIndex">
+            <img src="image/photo1.jpg" alt="moi sur une pirogue avec mes collegues">
+        </div>`
 })
+// document.querySelector(".containerIndex2").addEventListener("mouseover", function (bande2) {
+    
+//     document.querySelector(".texteIndex").style.display = "block";
+//     document.querySelector(".imgIndex img").style.display = "block";
+//     document.querySelector(".texteIndex2").style.display = "none";
+//     document.querySelector(".imgIndex2 img").style.display = "none";
+// })
 
 // document.querySelector("html").addEventListener("resize", function (inverse){})
