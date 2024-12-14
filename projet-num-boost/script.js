993f2fe1092ce598e3ed4283d9dff8367cab782e
NeroFiasconi <FIASCONI.NERO@GMAIL.COM>
2024-12-14 12:25:53 +0100

    essai interaction

---
+++ projet-num-boost/script.js
@@ -0,0 +1,20 @@
+document.querySelector(".texteIndex").addEventListener("mouseover", function (bande1) {
+    
+    document.querySelector(".texteIndex").style.display = "none";
+    document.querySelector(".asideIndex2 img").style.display = "none";
+    document.querySelector(".texteIndex2").style.display = "block";
+    document.querySelector(".boxIMG img").style.display = "block";
+            "J'en suis venu au développement web par l'intermédiaire du pentesting, en m'interessant à la lecture de puces RFID. Des outils comme le ProxMark sont opensource et mis à jour constament sur GitHubet j'ai trouvé ça fascninant.J'ai commencé par du langage COBOL. Avec youtube j'ai appris à faire de petits programmes de calculs, calculs d'imc, calculs de vitesse moyenne en course à pied. Je me suis ensuite tourné vers le langage Python, la syntaxe est simple et intuitive ce qui facilite l'apprentissage des bases à savoir les fonctions, les conditions, les boucles ainsi que l'utilisation des collections. J'ai appris énormément sur l'algorithmie.Mais ces programmes ne fonctionnaient que dans la console, je voulais faire des applications qu'on pourrait utiliser sur une page web.j'ai donc appris les bases HTML, CSS et Javascript avec Openclassroom afin des faire des pages dynamiques. J'ai pû aborder facilement l'algorithmie en Javascript car la logique est là même qu'en Python cependant, la séléction des éléments du DOM pour en faire des objets Javascript fut une découverte.";
+        // document.querySelector(".aside img").style.display = "none";
+        // document.querySelector(".box IMG img").style.display = "block";
+})
+
+document.querySelector(".texteIndex2").addEventListener("mouseover", function (bande2) {
+    
+    document.querySelector(".texteIndex2").style.display = "none";
+    document.querySelector(".boxIMG img").style.display = "none";
+    document.querySelector(".texteIndex").style.display = "block";
+    document.querySelector(".asideIndex2 img").style.display = "block";
+})
+
+// document.querySelector("html").addEventListener("resize", function (inverse){})
